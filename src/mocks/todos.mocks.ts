cbf9bf7d02ccee9ba7b0118beacf95a6049b7def
Freddy L <neo-ciber_94@hotmail.com>
2022-04-30 10:44:41 -0400

    Updated todo mock

--- src/mocks/todos.mocks.ts
+++ src/mocks/todos.mocks.ts
@@ -1,39 +1,23 @@
+import { nanoid } from "nanoid";
 import { Todo } from "../model/Todo";
 
 export const todosMocks: Todo[] = [
-  {
-    id: "323fa56c-5b6c-4be4-a5bb-bc91a3c510d7",
-    title: "Book a flight",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f4",
-    title: "Finish the book",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f5",
-    title: "Buy a milk",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f6",
-    title: "Buy a bread",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f7",
-    title: "Wash the dishes",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f8",
-    title: "Buy a water",
-    completed: false,
-  },
-  {
-    id: "c9b8f9e2-f8e3-4f0e-b8f4-b8f4b8f4b8f9",
-    title: "Buy white chocolate",
-    completed: false,
-  },
+  createTodo("Walk the dog"),
+  createTodo("Buy milk"),
+  createTodo("Buy eggs"),
+  createTodo("Buy bread"),
+  createTodo("Do the laundry"),
+  createTodo("Wash the dishes"),
+  createTodo("Clean the house"),
+  createTodo("Buy a new car"),
 ];
+
+function createTodo(title: string): Todo {
+  return {
+    id: nanoid(),
+    title,
+    completed: false,
+    version: 1,
+    createdAt: new Date(),
+  };
+}
